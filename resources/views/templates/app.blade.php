a881c0ab8a4f1fc017e186277228635bcc594787
David <dgudovic1@gmail.com>
2022-08-04 23:03:32 +0200

    Removed unused links from navbar

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -2,18 +2,12 @@
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <title>To-Do</title>
+  <title>Post It</title>
   @vite('resources/js/app.js')
 </head>
 <body class="bg-gray-600">
   <nav class="p-6 bg-white flex justify-between">
     <ul class='flex items-center'>
-      <li>
-        <a href='{{ route('home') }}' class='p-3'> Home </a>
-      </li>
-      <li>
-        <a href='{{ route('dashboard') }}' class='p-3'> Dashboard </a>
-      </li>
       <li>
         <a href='{{ route('posts') }}' class='p-3'> Posts </a>
       </li>
